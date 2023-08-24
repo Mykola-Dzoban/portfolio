777ce2964d55c12bc005ab2af6167b0b34592302
Mykola Dzoban <nick.dzoban@gmail.com>
2023-08-24 15:20:49 +0300

    fix hamburger

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,18 @@
 const hamburger=document.querySelector(".hamburger");
 const navMenu=document.querySelector(".nav-menu");
+const navItems = document.querySelectorAll('.nav-link');
 const body=document.body;
 
 hamburger.addEventListener("click", ()=>{
-    hamburger.classList.toggle("active");
+    hamburger.classList.toggle("active"); 
     navMenu.classList.toggle("active");
     body.classList.toggle("noscroll");
+})
+
+navItems.forEach((item)=>{
+    item.addEventListener('click', ()=>{
+        hamburger.classList.toggle("active"); 
+        navMenu.classList.toggle("active");
+        body.classList.toggle("noscroll");
+    })
 })
